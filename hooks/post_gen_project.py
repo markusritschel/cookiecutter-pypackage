4fe9992c629c1411e83fcbef300c553838aa9dcd
markusritschel <git@markusritschel.de>
2020-10-11 01:15:42 +0200

    Add call to add remote repo

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,3 +3,9 @@ import subprocess
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+
+subprocess.call(['git', 'remote', 'add', 'origin', 'https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git'])
+try:
+    subprocess.call(['git', 'push', '-u', 'remote', 'master'])
+except:
+    print("Couldn't reach remote repository https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git")
