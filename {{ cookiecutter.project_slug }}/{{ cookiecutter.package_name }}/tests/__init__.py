de8f2bb51ea77b38f0a81738cd78045a05a0fdfa
markusritschel <git@markusritschel.de>
2020-10-10 23:15:24 +0200

    Move ./tests/ one level up

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.package_name }}/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding utf-8 -*-
-#
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Author: {{ cookiecutter.full_name }}
-# eMail:  {{ cookiecutter.email }}
-# Date:   {% now 'local', '%Y-%m-%d' %}
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-#
