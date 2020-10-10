0527849f9608543c1eee1b8c89c15ec4676e6f82
markusritschel <git@markusritschel.de>
2020-10-10 23:16:00 +0200

    Add `logging` to {{ cookiecutter.package_name }}.py

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.package_name }}/{{ cookiecutter.package_name }}.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.package_name }}/{{ cookiecutter.package_name }}.py
@@ -9,6 +9,6 @@
 #
 from __future__ import absolute_import, division, print_function, with_statement
 
-import pandas as pd
-import numpy as np
-import xarray as xr
+import logging
+
+logger = logging.getLogger(__name__)
