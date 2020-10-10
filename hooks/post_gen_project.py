5d06d38f0f6baffcb4e9ecfb0c589df48532c1c5
markusritschel <git@markusritschel.de>
2020-10-11 01:23:19 +0200

    Do not automatically push to remote

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,8 +7,8 @@ subprocess.call(['git', 'commit', '-m', 'Initial commit'])
 print("Add remote repository https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git")
 subprocess.call(['git', 'remote', 'add', 'origin', 'https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git'])
 
-print("Try pushing to remote repository")
-try:
-    subprocess.call(['git', 'push', '-u', 'origin', 'master'])
-except:
-    print("Pushing failed! Couldn't reach remote repository https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git!")
+# print("Try pushing to remote repository")
+# try:
+#     subprocess.call(['git', 'push', '-u', 'origin', 'master'])
+# except:
+#     print("Pushing failed! Couldn't reach remote repository https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git!")
