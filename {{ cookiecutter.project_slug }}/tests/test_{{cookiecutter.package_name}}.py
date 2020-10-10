58045e47e6e8d7fba3ba34db48d484e36f955fae
markusritschel <git@markusritschel.de>
2020-10-10 23:14:16 +0200

    Move ./tests/ one level up

---
+++ {{ cookiecutter.project_slug }}/tests/test_{{cookiecutter.package_name}}.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding utf-8 -*-
+#
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: {{ cookiecutter.full_name }}
+# eMail:  {{ cookiecutter.email }}
+# Date:   {% now 'local', '%Y-%m-%d' %}
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import pytest
+
+@pytest.fixture(scope="module")
+def fixture(request):
+    pass
+
+
+def test_app(fixture):
+    """Test based on the fixture"""
+    assert fixture.title == str
+
+
+def test_glob(global_fixture):
+    assert global_fixture == True
