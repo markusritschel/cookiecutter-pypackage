5e3a0a679ec4d1e96fa0b7f5a5f48973043f004e
markusritschel <git@markusritschel.de>
2020-10-11 01:19:11 +0200

    Correct typo

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,6 +6,6 @@ subprocess.call(['git', 'commit', '-m', 'Initial commit'])
 
 subprocess.call(['git', 'remote', 'add', 'origin', 'https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git'])
 try:
-    subprocess.call(['git', 'push', '-u', 'remote', 'master'])
+    subprocess.call(['git', 'push', '-u', 'origin', 'master'])
 except:
     print("Couldn't reach remote repository https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git")
