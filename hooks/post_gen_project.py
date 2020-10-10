bfecaf0be23eecaab0ba24d177aea94cba4658f4
markusritschel <git@markusritschel.de>
2020-10-11 01:21:21 +0200

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,8 +4,11 @@ subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
 
+print("Add remote repository https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git")
 subprocess.call(['git', 'remote', 'add', 'origin', 'https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git'])
+
+print("Try pushing to remote repository")
 try:
     subprocess.call(['git', 'push', '-u', 'origin', 'master'])
 except:
-    print("Couldn't reach remote repository https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git")
+    print("Pushing failed! Couldn't reach remote repository https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}.git!")
