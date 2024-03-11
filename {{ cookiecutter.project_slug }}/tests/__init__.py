bbb3326e4d83868cb638d6f4aa25241f448ed449
markusritschel <git@markusritschel.de>
2024-03-11 16:04:03 +0100

    Add preliminary test files

---
+++ {{ cookiecutter.project_slug }}/tests/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding utf-8 -*-
+#
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: {{ cookiecutter.project_author }}
+# eMail:  {{ cookiecutter.email }}
+# Date:   {% now 'local', '%Y-%m-%d' %}
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
---
+++ {{ cookiecutter.project_slug }}/tests/conftest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding utf-8 -*-
+#
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Author: {{ cookiecutter.project_author }}
+# eMail:  {{ cookiecutter.email }}
+# Date:   {% now 'local', '%Y-%m-%d' %}
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#
+import pytest
+
+
+@pytest.fixture()
+def global_fixture(request):
+    return 'Test'
