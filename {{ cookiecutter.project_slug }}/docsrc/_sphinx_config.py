20cf6c48bea980619ae7df80a2047e9a5327acd1
markusritschel <git@markusritschel.de>
2024-06-11 15:39:40 +0200

    Rename docsrc to docs

--- {{ cookiecutter.project_slug }}/docsrc/_sphinx_config.py
+++
@@ -1,8 +0,0 @@
-def skip_logger(app, what, name, obj, skip, options):
-    if obj.name.startswith('log'):
-        skip = True
-    return skip
-
-
-def setup(sphinx):
-    sphinx.connect("autoapi-skip-member", skip_logger)
