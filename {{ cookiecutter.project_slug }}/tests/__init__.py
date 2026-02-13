d241f8465f751ada2b1d42673fd6979a3be62003
Markus Ritschel <git@markusritschel.de>
2026-02-13 13:24:41 +0100

    Cleanup

--- {{ cookiecutter.project_slug }}/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding utf-8 -*-
-#
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# Author: {{ cookiecutter.project_author }}
-# eMail:  {{ cookiecutter.email }}
-# Date:   {% now 'local', '%Y-%m-%d' %}
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-#
